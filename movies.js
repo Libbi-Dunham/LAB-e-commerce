83dc191215fb65e6f1ac492ab4e28c0b1870f242
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-11 13:21:09 -0700

    deleted bootstramp template and added my product.js

---
+++ movies.js
@@ -0,0 +1,35 @@
+export const movies = [
+    {
+        id: '1',
+        name: 'movie1',
+        img: 'picture',
+        reviews: 5,
+        price: 6
+    },
+    {
+        id: '2',
+        name: 'movie2',
+        img: 'picture',
+        reviews: 2,
+        price: 6 },
+    {
+        id: '3',
+        name: 'movie3',
+        img: 'picture',
+        reviews: 4,
+        price: 6 },
+    {
+        id: '4',
+        name: 'movie4',
+        img: 'picture',
+        reviews: 3,
+        price: 6
+    },
+    {
+        id: '5',
+        name: 'movie5',
+        img: 'picture',
+        reviews: 5,
+        price: 6
+    }
+];

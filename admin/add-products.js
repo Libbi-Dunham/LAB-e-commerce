03a90a507e8db9890abef9dc8ed50ee964b3919c
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-18 15:38:47 -0700

    fixed data error

--- admin/add-products.js
+++ admin/add-products.js
@@ -1,10 +1,19 @@
-import { addProduct } from '../utils';
+import { addProduct } from '../utils.js';
 
 const form = document.getElementById('product-form');
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
+    const data = new FormData(form);
     const newMovies = {
+        id: data.get('id'),
+        name: data.get('name'),
+        img: data.get('img'),
+        reviews: data.get('reviews'),
+        price: Number(data.get('price'))
     
-    }
-})
+    };
+    console.log(newMovies);
+    addProduct(newMovies);
+    alert('Movie Rented!');
+});
